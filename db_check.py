a4135adb205ede21b3d6bb9363feb76c75257b3d
emirodriguez <emiliano.rodriguez26@gmail.com>
2021-01-21 15:43:39 -0300

    Try fix

--- db_check.py
+++ db_check.py
@@ -25,14 +25,15 @@ def check_result_send_mess():
     results.append(resultsPage3)
     
     for item in results:
-        car_exists = cars_db.execute('SELECT car FROM cars WHERE car = %s', [item['href']])
-        
-        if len(cars_db.fetchall()) != 1:
-            mess_content = 'https://www.maspocovendo.com' +  item['href']
-            message.send_message(chat_id, mess_content)
-            cars_db.execute('INSERT INTO cars (car) VALUES (%s);', [item['href']])
-            conn.commit()
-        else:
-            continue
+        if href in item 
+            car_exists = cars_db.execute('SELECT car FROM cars WHERE car = %s', [item['href']])
+            
+            if len(cars_db.fetchall()) != 1:
+                mess_content = 'https://www.maspocovendo.com' +  item['href']
+                message.send_message(chat_id, mess_content)
+                cars_db.execute('INSERT INTO cars (car) VALUES (%s);', [item['href']])
+                conn.commit()
+            else:
+                continue
             
     cars_db.close()
