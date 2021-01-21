1189d7563806c2e5acc2d700adcf9aae35ddf41f
emirodriguez <emiliano.rodriguez26@gmail.com>
2021-01-21 15:00:29 -0300

    Fix

--- scheduler.py
+++ scheduler.py
@@ -1,9 +1,12 @@
 import schedule
 import db_check
+import time
 
 # schedule crawler
+schedule
 schedule.every(30).minutes.do(db_check.check_result_send_mess)
 
 # run script infinitely
 while True:
     schedule.run_pending()
+    time.sleep(1)
